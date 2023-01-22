ca84937cd1120b3ad9cf9741b4efccba7ef83edd
sakis-anast <sak.anastasopoulos@gmail.com>
2023-01-22 13:48:56 +0200

    receiving msgs functionality and display them

--- client/src/components/ChatArea.jsx
+++ client/src/components/ChatArea.jsx
@@ -2,21 +2,24 @@ import Logout from "./Logout";
 import axios from "axios";
 import MessageBar from "./MessageBar";
 import "../styles/ChatArea.scss";
-import ChatMessages from "./ChatMessages";
 import React , {useEffect , useState} from "react";
 function ChatArea({currentChat , user }){
     const [messages, setMessages] = useState([]);
 
-    useEffect(()=>{
-        async function getMessages(){
-          const result=  await axios.get("http://localhost:3001/message",{
-            from : user._id,
-            to : currentChat._id,
-        });
-        setMessages(result.data);}
-        getMessages();
-        console.log(messages)
-    },[currentChat])
+    useEffect( () => {
+      async function getMessages(){
+      const data = await JSON.parse(
+        localStorage.getItem("user")
+      );
+      const response = await axios.post("http://localhost:3001/message/get", {
+        from: data._id,
+        to: currentChat._id,
+      });
+      setMessages(response.data);
+    }
+    getMessages()
+  }, [currentChat]);
+ 
 const handleMessage =async (msg)=>{
     await axios.post("http://localhost:3001/message",{
         from : user._id,
@@ -28,7 +31,7 @@ const handleMessage =async (msg)=>{
 
     return(
         <>
-        {currentChat &&(<div className="chat-container">
+        {currentChat && (<div className="chat-container">
             <div className="chatHeader">
                 <div className="chatDetails">
                     <div className="chatUsername">
@@ -43,10 +46,10 @@ const handleMessage =async (msg)=>{
             <div >
               <div
                 className={`message ${
-                  message.fromSelf ? "sended" : "recieved"
+                  message.fromSelf ? "send" : "receive"
                 }`}
               >
-                <div className="content ">
+                <div className="content">
                   <p>{message.message}</p>
                 </div>
               </div>
--- server/routers/messageRoute.js
+++ server/routers/messageRoute.js
@@ -1,7 +1,7 @@
 const messageController = require ("../controllers/messageController")
 const router = require("express").Router()
 
-router.get("/" , messageController.getMessage)
+router.post("/get" , messageController.getMessage)
 router.post("/" , messageController.postMessage)
 router.put("/:id" , messageController.updateMessage)
 router.delete("/:id" , messageController.deleteMessage)
