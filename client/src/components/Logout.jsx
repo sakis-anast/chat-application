543009f834f4e9ead6e69c490a2644dc3c5cc902
sakis-anast <sak.anastasopoulos@gmail.com>
2023-01-23 16:48:02 +0200

    clean up frontEnd

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -3,7 +3,7 @@ import "../styles/Logout.scss";
 
 function Logout() {
   const navigate = useNavigate();
-
+//remove everything from the localStorage data that we were using ang go to sign up
   const logoutHandle = () => {
     localStorage.clear();
     navigate("/");
