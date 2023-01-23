acd36fa62e9e0ef158757144da13d501eabb0f6b
sakis-anast <sak.anastasopoulos@gmail.com>
2023-01-23 16:05:32 +0200

    server clean up

--- server/routers/messageRoute.js
+++ server/routers/messageRoute.js
@@ -3,8 +3,6 @@ const router = require("express").Router()
 
 router.post("/get" , messageController.getMessage)
 router.post("/" , messageController.postMessage)
-router.put("/:id" , messageController.updateMessage)
-router.delete("/:id" , messageController.deleteMessage)
 
 
 module.exports =  router;
